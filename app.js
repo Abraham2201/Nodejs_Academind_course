9cbd3fb5b1ed3d3afd360249be9edc7e0d7dc224
Abraham RM <ing.abraham.rod.mon@gmail.com>
2024-12-05 20:24:43 -0500

    adding pug dynamic templates

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ const parser = require('body-parser');
 const path = require('path');
 const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const { engine } = require('express-handlebars');
 
 const application = express(); //This constant can be used as a handler. so we can pass it into the http.createServer(application);
+
+application.set('view engine', 'pug'); //set() heps us to set a global configuration value.(keys, configuration items)
+//pug is supported out of the box.
+application.set('views', 'views'); // this is mainly used when our views folder is named differently or in another location.
 
 // application.use((req, res, next) => {
 //     console.log('In the middleware');
 //     next(); //Allows the request to continue to the next middleware in line
 // });
 
-application.use(parser.urlencoded({ extended: true }));
+application.use(parser.urlencoded({ extended: false }));
 application.use(express.static(path.join(__dirname, 'public')));
 application.use('/admin', adminData.routes); // when we add at the beginning the /admin, all routes having /admin will be handled by the adminRouter. It's a filter.
 application.use(shopRoutes);
--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,14 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     // console.log('In the middleware main page');
     // res.send('<h1>The "main path / " page here!</h1>');
-    // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html')); //Good way in which we go up one directory and then we access to the views
+    // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html')); //Good way in which we go up one directory and then we access the views
     console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+    //render is a method provided by express js so we can use the default template engine that we setted before
+    const products = adminData.products;
+    res.render('shop', { prods: products, docTitle: 'Dynamic Shop' });
+
 });
 
 module.exports = router;
